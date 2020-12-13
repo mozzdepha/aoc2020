d3df8a88d6ecf86c7e118b8c109649c5b63c6c3b
mozzdepha <Majest1c!$%>
2020-12-13 08:37:45 +0000

    Day13 pt1

--- src/com/mozzdev/Day13.java
+++ src/com/mozzdev/Day13.java
@@ -2,7 +2,9 @@ package com.mozzdev;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day13 {
     private List<String> list;
@@ -40,7 +42,46 @@ public class Day13 {
     }
 
 
+    private int target;
     public void challenge1() {
+        target = Integer.parseInt(list.get(0));
+        int minWait = 10000000;
+        int targetBus = 0;
+        List<Integer> busses = parseBusses(list.get(1));
+        Map<Integer, Integer> remainders = new HashMap<Integer, Integer>();
+        for (Integer bus : busses) {
+//            remainders.put(bus, target % bus);
+            int wait = findWait(target, bus);
+            System.out.println("Bus: " + bus + "; remainder: " + wait);
+            if (wait < minWait) {
+                minWait = wait;
+                targetBus = bus;
+            }
+        }
+        System.out.println("Bus: " + targetBus + "; remainder: " + minWait + "; Result: " + (targetBus * minWait));
 
+    }
+
+    private int findWait(int target, int bus) {
+        int x=1;
+        int goal=0;
+        while (true) {
+            goal+=bus;
+            if (goal > target) {
+                return goal-target;
+            }
+            x++;
+        }
+    }
+
+    private List<Integer> parseBusses(String buss) {
+        String[] chars = buss.split(",");
+        List<Integer> output = new ArrayList<Integer>();
+        for (int x=0; x< chars.length; x++) {
+            if (!chars[x].equals("x")) {
+                output.add(Integer.parseInt(chars[x]));
+            }
+        }
+        return output;
     }
 }
