30421b1fa0f7b58b3449f050b5a60c9f3dff12dc
Tim Moss <tim.moss@mac.com>
2022-12-02 06:24:32 -0700

    AOC Day 2 complete

--- src/com/mozzdev/adventofcode/aoc2022/Day1.java
+++ src/com/mozzdev/adventofcode/aoc2022/Day1.java
@@ -21,13 +21,11 @@ public class Day1 extends AocDay2022 {
 
     public void challenge1() {
         initNumbers();
-        Collections.sort(numbers);
         System.out.println(numbers.get(numbers.size()-1));
     }
 
     public void challenge2() {
         initNumbers();
-        Collections.sort(numbers);
         int totalCalories = numbers.get(numbers.size()-1);
         totalCalories += numbers.get(numbers.size()-2);
         totalCalories += numbers.get(numbers.size()-3);
@@ -45,35 +43,8 @@ public class Day1 extends AocDay2022 {
                 currentTotal += Integer.parseInt(numString);
             }
         }
+        Collections.sort(numbers);
     }
-
-    private void checkMatchFor2020(int num) {
-        int target = 2020 - num;
-//        System.out.println("Target: " + num + "; looking for: " + target);
-        for (Integer candidate : numbers) {
-            if (target == candidate.intValue()) {
-                System.out.println("Found match! " + num + " matches with " + candidate.intValue());
-                System.out.println("Result: " + (num*candidate.intValue()));
-            }
-        }
-    }
-    private void checkMatchFor20202(int num) {
-        for (Integer candidate : numbers) {
-            if (num+candidate < 2020) {
-                for (Integer candidate2 : numbers) {
-                    if (num+candidate+candidate2 == 2020) {
-                        System.out.println("Found match! " + num + " - " + candidate + " - " + candidate2 + "; answer = " + num*candidate*candidate2);
-                        break;
-                    } else {
-//                        System.out.println("No match:" + num + "+" + candidate + "+" + candidate2 + "=" + (num + candidate + candidate2));
-                    }
-                }
-            }
-
-
-        }
-    }
-
 
 
 }
---
+++ src/com/mozzdev/adventofcode/aoc2022/Day2.java
@@ -0,0 +1,80 @@
+package com.mozzdev.adventofcode.aoc2022;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day2 extends AocDay2022 {
+
+
+    private List<Round> rounds;
+
+    public static void main(String[] args) {
+        new Day2().run();
+    }
+
+    public String getInputFilename() {
+        return "inputs2.txt";
+    }
+
+    public void challenge1() {
+        initRounds();
+        int totalScore = 0;
+        int roundCount = 0;
+        for (Round round : rounds) {
+            totalScore += round.getScore1();
+            roundCount++;
+        }
+        System.out.println(totalScore);
+    }
+
+    public void challenge2() {
+        initRounds();
+        int totalScore = 0;
+        int roundCount = 0;
+        for (Round round : rounds) {
+            totalScore += round.getScore2();
+            roundCount++;
+        }
+        System.out.println(totalScore);
+    }
+
+    private void initRounds() {
+        rounds = new ArrayList<Round>();
+        for (String numString: list) {
+            rounds.add(new Round(numString));
+        }
+    }
+
+
+    private class Round {
+
+        private int score1 =0;
+        private int score2 =0;
+
+        private String source;
+
+        public Round(String source) {
+            this.source = source;
+            switch (source) {
+                case "A X" : this.score1 =4; this.score2 =3; break;
+                case "A Y" : this.score1 =8; this.score2 =4; break;
+                case "A Z" : this.score1 =3; this.score2 =8; break;
+                case "B X" : this.score1 =1; this.score2 =1; break;
+                case "B Y" : this.score1 =5; this.score2 =5; break;
+                case "B Z" : this.score1 =9; this.score2 =9; break;
+                case "C X" : this.score1 =7; this.score2 =2; break;
+                case "C Y" : this.score1 =2; this.score2 =6; break;
+                case "C Z" : this.score1 =6; this.score2 =7; break;
+                default: System.out.println("unknown char");
+            }
+        }
+
+
+        public int getScore1() {
+            return score1;
+        }
+        public int getScore2() {
+            return score2;
+        }
+    }
+}
