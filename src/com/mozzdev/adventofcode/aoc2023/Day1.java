b8434a1394055ab55268208e265466972f22fa63
Tim Moss <tim.moss@mac.com>
2023-12-01 15:30:11 -0500

    AOC2023 Day1

--- src/com/mozzdev/adventofcode/aoc2023/Day1.java
+++ src/com/mozzdev/adventofcode/aoc2023/Day1.java
@@ -1,13 +1,12 @@
 package com.mozzdev.adventofcode.aoc2023;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Day1 extends AocDay2023 {
 
-
-    private List<Integer> numbers;
+    private static final String[] INT_STRINGS = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    private static final int[] INT_VALUES = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     public static void main(String[] args) {
         new Day1().run();
@@ -18,31 +17,71 @@ public class Day1 extends AocDay2023 {
     }
 
     public void challenge1() {
-        initNumbers();
-        System.out.println(numbers.get(numbers.size()-1));
+        int total = 0;
+        for (String current: list) {
+            total += getTotal(current);
+        }
+        System.out.println(total);
+    }
+
+
+    private int getTotal(String current) {
+        return Integer.parseInt(getFirstInt(current) + getLastInt(current));
+    }
+
+    private String getFirstInt(String current) {
+        for (int x=0; x<current.length(); x++) {
+
+            String currentS = current.substring(x, x + 1);
+            if (isDigit(currentS)) {
+                return currentS;
+            }
+        }
+        return "";
+    }
+
+    private String getLastInt(String current) {
+        String input = new StringBuilder(current).reverse().toString();
+        return getFirstInt(input);
+    }
+
+    private boolean isDigit(String s) {
+        Character currentChar = s.charAt(0);
+        return (currentChar.hashCode() >= 48 && currentChar.hashCode() <=57);
     }
 
     public void challenge2() {
-        initNumbers();
-        int totalCalories = numbers.get(numbers.size()-1);
-        totalCalories += numbers.get(numbers.size()-2);
-        totalCalories += numbers.get(numbers.size()-3);
-        System.out.println(totalCalories);
+        int total = 0;
+        for (String current: list) {
+            List<Integer> ints = getInts(current);
+            String result = "" + ints.get(0) + ints.get(ints.size()-1);
+            total += Integer.parseInt(result);
+        }
+        System.out.println(total);
     }
 
-    private void initNumbers() {
-        numbers = new ArrayList<>();
-        int currentTotal = 0;
-        for (String numString: list) {
-            if (numString.equals("")) {
-                numbers.add(currentTotal);
-                currentTotal = 0;
+    private List<Integer> getInts(String input) {
+        List<Integer> ints = new ArrayList<>();
+        for (int x=0; x<input.length(); x++) {
+            if (isDigit(input.substring(x, x+1))) {
+                ints.add(Integer.parseInt(input.substring(x, x+1)));
             } else {
-                currentTotal += Integer.parseInt(numString);
+                int foundValue = getIntegerForString(input.substring(x));
+                if (foundValue >=0) {
+                    ints.add(foundValue);
+                }
             }
         }
-        Collections.sort(numbers);
+        return ints;
     }
 
 
+    private int getIntegerForString(String input) {
+        for (int x=0; x<INT_STRINGS.length; x++) {
+            if (input.startsWith(INT_STRINGS[x])) {
+                return INT_VALUES[x];
+            }
+        }
+        return -1;
+    }
 }
---
+++ src/com/mozzdev/adventofcode/aoc2023/Day2.java
@@ -0,0 +1,22 @@
+package com.mozzdev.adventofcode.aoc2023;
+
+public class Day2 extends AocDay2023 {
+
+
+    public static void main(String[] args) {
+        new Day2().run();
+    }
+
+    public String getInputFilename() {
+        return "inputs2.txt";
+    }
+
+    public void challenge1() {
+
+    }
+
+    public void challenge2() {
+
+    }
+
+}
