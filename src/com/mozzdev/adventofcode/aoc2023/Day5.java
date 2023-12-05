28e2515c0213ca633402cb2f2850c86e46363312
Tim Moss <tim.moss@mac.com>
2023-12-05 14:46:09 +0000

    AOC2023 Day5

--- src/com/mozzdev/adventofcode/aoc2023/Day5.java
+++ src/com/mozzdev/adventofcode/aoc2023/Day5.java
@@ -1,5 +1,11 @@
 package com.mozzdev.adventofcode.aoc2023;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Day5 extends AocDay2023 {
 
     public static void main(String[] args) {
@@ -11,18 +17,116 @@ public class Day5 extends AocDay2023 {
     }
 
     public void challenge1() {
-        int total = 0;
-        for (String current: list) {
+        initData();
+        double lowestLocation = 100000000000d;
+        for (Double seed: seeds) {
             // do something
+            double location = getLocation(seed);
+            lowestLocation=Math.min(lowestLocation, location);
         }
-        System.out.println(total);
+        System.out.println(NumberFormat.getInstance().format(lowestLocation));
     }
 
-    public void challenge2() {
-        int total = 0;
-        for (String current: list) {
-            // do something
+    private double getLocation(Double seed) {
+        Mapping m = mappings.get("seed");
+        double input=seed;
+        while (!m.destination.equals("location")) {
+            input = m.getNextDestination(input);
+            String nextDest = m.destination;
+            m=mappings.get(m.destination);
+            if (m==null) {
+                System.out.println("top");
+            }
         }
-        System.out.println(total);
+        return m.getNextDestination(input);
+    }
+
+
+    public void challenge2() {
+        double lowestLocation = 100000000000d;
+        for (int x=0; x<seeds.size()-1; x=x+2) {
+            Double seed = seeds.get(x);
+            Double range = seeds.get(x+1);
+            for (double y=0; y<range; y++) {
+                // do something
+                double location = getLocation(seed+y);
+                lowestLocation = Math.min(lowestLocation, location);
+            }
+        }
+        System.out.println(NumberFormat.getInstance().format(lowestLocation));
+    }
+
+    private List<Double> seeds;
+    private Map<String, Mapping> mappings;
+
+    private class Mapping {
+        private String source, destination;
+
+
+        private class MapEntry {
+            double destinationRange, sourceRange, rangeLength;
+
+            public MapEntry(double destinationRange, double sourceRange, double rangeLength) {
+                this.destinationRange = destinationRange;
+                this.sourceRange = sourceRange;
+                this.rangeLength = rangeLength;
+            }
+        }
+
+        private List<MapEntry> mapEntries;
+
+        public Mapping() {
+            mapEntries = new ArrayList<>();
+        }
+
+        public void add(double destinationRange, double sourceRange, double rangeLength) {
+            mapEntries.add(new MapEntry(destinationRange, sourceRange, rangeLength));
+        }
+
+        public double getNextDestination(double source) {
+            for (MapEntry me: mapEntries) {
+                if (inRange(source, me)) {
+                    return me.destinationRange + (source - me.sourceRange);
+                }
+            }
+            // no explit mapping - return source
+            return source;
+        }
+
+        private boolean inRange(double source, MapEntry me) {
+            return source >= me.sourceRange && source <= (me.sourceRange + me.rangeLength);
+        }
+    }
+
+    private Mapping current;
+    private void initData() {
+        mappings = new HashMap<>();
+        for (String s : list) {
+            if (s.startsWith("seeds:")) {
+                initSeeds(s);
+            } else if (s.contains("-to-")) {
+                String source = s.substring(0, s.indexOf("-to-"));
+                String destination = s.substring(s.indexOf("-to-") + 4, s.indexOf(" map"));
+                current = new Mapping();
+                current.source = source;
+                current.destination = destination;
+            } else if (s.isEmpty()) {
+                // do nothing - next line should be a new Mapping
+                if (current != null) {
+                    mappings.put(current.source, current);
+                }
+            } else {
+                String[] tokens = s.split(" ");
+                current.add(Double.parseDouble(tokens[0]), Double.parseDouble(tokens[1]), Double.parseDouble(tokens[2]));
+            }
+        }
+    }
+
+    private void initSeeds(String s) {
+        seeds = new ArrayList<>();
+        String[] tokens = s.split(":")[1].trim().split(" ");
+        for (int x=0; x<tokens.length; x++) {
+            seeds.add(Double.parseDouble(tokens[x]));
+        }
     }
 }
---
+++ src/com/mozzdev/adventofcode/aoc2023/Day6.java
@@ -0,0 +1,28 @@
+package com.mozzdev.adventofcode.aoc2023;
+
+public class Day6 extends AocDay2023 {
+
+    public static void main(String[] args) {
+        new Day6().run();
+    }
+
+    public String getInputFilename() {
+        return "inputs6.txt";
+    }
+
+    public void challenge1() {
+        int total = 0;
+        for (String current: list) {
+            // do something
+        }
+        System.out.println(total);
+    }
+
+    public void challenge2() {
+        int total = 0;
+        for (String current: list) {
+            // do something
+        }
+        System.out.println(total);
+    }
+}
